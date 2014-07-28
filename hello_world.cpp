6529e8ec4c997f15ec418131bf8c788c35abcb1b
yeahframeoff <anthyef@gmail.com>
2014-07-28 07:11:18 +0300

    Added time output

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 int main(void)
 {
 	cout << "Hello, world!!!" << endl;
+	cout << "Now is " << time(0) << " seconds" << endl;
 	cin.ignore();
 	return 0;
 }
